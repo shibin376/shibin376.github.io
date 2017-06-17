5035d54053c1213d1b9ab227100fba45a39e4d53
zuipiaoliangdenvhai <415528382@qq.com>
2017-06-18 04:56:23 +0800

    Update Main.js

--- assets/Main.js
+++ assets/Main.js
@@ -87,7 +87,7 @@ function reset() {
 
 	// color theme
 	theme = themes[ Math.random() * themes.length >> 0 ];
-	document.body.style[ 'backgroundColor' ] = theme[ 0 ];
+	document.body.style[ 'backgroundColor' ] = "#FFFFFF";
 
 	bodies = [];
 	elements = [];
@@ -219,7 +219,7 @@ function createInstructions() {
 
 	text = document.createElement( 'div' );
 	text.onSelectStart = null;
-	text.innerHTML = '<span style="color:' + theme[0] + ';font-size:26px;">玩法介绍：</span><br /><span style="font-size:15px;"><br />1. 随意拖动圆球；<br />2.点击页面背景；<br />3. 晃动浏览器；<br />4. 双击页面背景；<br />5. 按住鼠标左键。</span>';
+	text.innerHTML = '<span style="color:' + theme[0] + ';font-size:26px;">玩法介绍：</span><br /><span style="font-size:15px;"><br />你站在桥上看风景<br />看风景的人在楼上看你<br />明月装饰了你的窗子<br />你装饰了别人的梦</span>';
 	text.style.color = theme[1];
 	text.style.position = 'absolute';
 	text.style.left = '0px';
